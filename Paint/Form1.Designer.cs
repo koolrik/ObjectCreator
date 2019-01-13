60ee96e20d772d0f2e3a4e7a4dbbbe281d41ce78
Rikesh Karki <44747589+koolrik@users.noreply.github.com>
2019-01-13 18:46:50 +0545

    Updated Icon and features.

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -95,21 +95,21 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -374,7 +374,8 @@
             // 
             this.btnChooseColor.BackColor = System.Drawing.Color.Black;
             this.btnChooseColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnChooseColor.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.btnChooseColor.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnChooseColor.FlatAppearance.BorderSize = 3;
             this.btnChooseColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnChooseColor.Location = new System.Drawing.Point(678, 15);
             this.btnChooseColor.Name = "btnChooseColor";
